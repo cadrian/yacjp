e146fc4dc0717afdd63402af5a4edc916d738d11
Cyril Adrian <cyril.adrian@gmail.com>
2012-06-17 21:32:02 +0200

    JSON arrays

--- include/json_value.h
+++ include/json_value.h
@@ -1,18 +1,18 @@
 /*
-    This file is part of YACJP.
+  This file is part of YACJP.
 
-    YacJP is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, version 3 of the License.
+  YacJP is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3 of the License.
 
-    YacJP is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  YacJP is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with YacJP.  If not, see <http://www.gnu.org/licenses/>.
- */
+  You should have received a copy of the GNU General Public License
+  along with YacJP.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef _YACJP_JSON_VALUE_H_
 #define _YACJP_JSON_VALUE_H_
@@ -33,8 +33,8 @@ typedef struct json_visitor json_visitor_t;
 
 typedef struct json_object_field json_object_field_t;
 struct json_object_field {
-   const char *key;
-   json_value_t *value;
+     const char *key;
+     json_value_t *value;
 };
 
 typedef void                 (*json_object_accept_fn   ) (json_object_t *this, json_visitor_t *visitor);
@@ -45,30 +45,32 @@ typedef void                 (*json_object_set_value_fn) (json_object_t *this, c
 typedef void                 (*json_object_del_value_fn) (json_object_t *this, const char *key);
 
 struct json_object {
-   json_object_accept_fn    accept   ;
-   json_object_count_fn     count    ;
-   json_object_get_field_fn get_field;
-   json_object_get_value_fn get_value;
-   json_object_set_value_fn set_value;
-   json_object_del_value_fn del_value;
+     json_object_accept_fn    accept   ;
+     json_object_count_fn     count    ;
+     json_object_get_field_fn get_field;
+     json_object_get_value_fn get_value;
+     json_object_set_value_fn set_value;
+     json_object_del_value_fn del_value;
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 typedef void          (*json_array_accept_fn) (json_array_t *this, json_visitor_t *visitor);
 typedef unsigned int  (*json_array_count_fn ) (json_array_t *this);
-typedef json_value_t *(*json_array_get_fn   ) (json_array_t *this, int i);
-typedef void          (*json_array_set_fn   ) (json_array_t *this, int i, json_value_t *value);
-typedef void          (*json_array_add_fn   ) (json_array_t *this,        json_value_t *value);
-typedef void          (*json_array_del_fn   ) (json_array_t *this, int i);
+typedef json_value_t *(*json_array_get_fn   ) (json_array_t *this, int index);
+typedef void          (*json_array_set_fn   ) (json_array_t *this, int index, json_value_t *value);
+typedef void          (*json_array_ins_fn   ) (json_array_t *this, int index, json_value_t *value);
+typedef void          (*json_array_add_fn   ) (json_array_t *this,            json_value_t *value);
+typedef void          (*json_array_del_fn   ) (json_array_t *this, int index);
 
 struct json_array {
-   json_array_accept_fn accept;
-   json_array_count_fn  count ;
-   json_array_get_fn    get   ;
-   json_array_set_fn    set   ;
-   json_array_add_fn    add   ;
-   json_array_del_fn    del   ;
+     json_array_accept_fn accept;
+     json_array_count_fn  count ;
+     json_array_get_fn    get   ;
+     json_array_set_fn    set   ;
+     json_array_set_fn    ins   ;
+     json_array_add_fn    add   ;
+     json_array_del_fn    del   ;
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -78,9 +80,9 @@ typedef char *(*json_string_get_fn   ) (json_string_t *string);
 typedef void  (*json_string_set_fn   ) (json_string_t *string, char *format, ...) __attribute__((format(printf, 2, 3)));
 
 struct json_string {
-   json_string_accept_fn accept;
-   json_string_get_fn    get   ;
-   json_string_set_fn    set   ;
+     json_string_accept_fn accept;
+     json_string_get_fn    get   ;
+     json_string_set_fn    set   ;
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -92,26 +94,26 @@ typedef double (*json_number_to_double_fn) (json_number_t *this);
 typedef void   (*json_number_set_fn      ) (json_number_t *this, int integral, int decimal, int decimal_exp, int exp);
 
 struct json_number {
-   json_number_accept_fn    accept   ;
-   json_number_is_int_fn    is_int   ;
-   json_number_to_int_fn    to_int   ;
-   json_number_to_double_fn to_double;
-   json_number_set_fn       set      ;
+     json_number_accept_fn    accept   ;
+     json_number_is_int_fn    is_int   ;
+     json_number_to_int_fn    to_int   ;
+     json_number_to_double_fn to_double;
+     json_number_set_fn       set      ;
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 struct json_value {
-   void (*accept) (json_value_t *this, json_visitor_t *visitor);
+     void (*accept) (json_value_t *this, json_visitor_t *visitor);
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 struct json_visitor {
-   void (*visit_object) (json_visitor_t *this, json_object_t *visited);
-   void (*visit_array)  (json_visitor_t *this, json_array_t  *visited);
-   void (*visit_string) (json_visitor_t *this, json_string_t *visited);
-   void (*visit_number) (json_visitor_t *this, json_number_t *visited);
+     void (*visit_object) (json_visitor_t *this, json_object_t *visited);
+     void (*visit_array)  (json_visitor_t *this, json_array_t  *visited);
+     void (*visit_string) (json_visitor_t *this, json_string_t *visited);
+     void (*visit_number) (json_visitor_t *this, json_number_t *visited);
 };
 
 /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
---
+++ src/json_array.c
@@ -0,0 +1,119 @@
+/*
+  This file is part of YACJP.
+
+  YacJP is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, version 3 of the License.
+
+  YacJP is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with YacJP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "json_value.h"
+
+struct json_array_impl {
+     struct json_array fn;
+
+     int capacity;
+     int count;
+     json_value_t **values;
+};
+
+static void accept(struct json_array_impl *this, json_visitor_t *visitor) {
+     visitor->visit_array(visitor, (json_array_t*)this);
+}
+
+static void grow(struct json_array_impl *this) {
+     int new_capacity;
+     json_value_t **new_values;
+     if (this->capacity == 0) {
+          new_capacity = 4;
+          new_values = (json_value_t **)malloc(new_capacity * sizeof(json_value_t **));
+     }
+     else {
+          new_capacity = this->capacity * 2;
+          new_values = (json_value_t **)malloc(new_capacity * sizeof(json_value_t **));
+          memset(new_values + this->capacity, 0, this->capacity);
+          memcpy(new_values, this->values, this->capacity);
+          free(this->values);
+     }
+     this->capacity = new_capacity;
+     this->values = new_values;
+}
+
+static unsigned int count(struct json_array_impl *this) {
+     return this->count;
+}
+
+static json_value_t *get(struct json_array_impl *this, int index) {
+     json_value_t *result = NULL;
+     if (index >= 0 && index < this->count) {
+          result = this->values[index];
+     }
+     return result;
+}
+
+static void set(struct json_array_impl *this, int index, json_value_t *value) {
+     if (index < this->count) {
+          this->values[index] = value;
+     }
+     else {
+          while (index > this->capacity) {
+               grow(this);
+          }
+          this->values[index] = value;
+          this->count = index + 1;
+     }
+}
+
+static void ins(struct json_array_impl *this, int index, json_value_t *value) {
+     if (index >= this->count) {
+          set(this, index, value);
+     }
+     else {
+          if (this->count == this->capacity) {
+               grow(this);
+          }
+          while (index > this->capacity) {
+               grow(this);
+          }
+          memmove(this->values + index + 1, this->values + index, this->count - index);
+          this->values[index] = value;
+          this->count++;
+     }
+}
+
+static void add(struct json_array_impl *this, json_value_t *value) {
+     set(this, this->count, value);
+}
+
+static void del(struct json_array_impl *this, int index) {
+     if (index >= 0 && index < this->count) {
+          memmove(this->values + index, this->values + index + 1, this->count - index - 1);
+          this->count--;
+     }
+}
+
+__PUBLIC__ json_value_t *json_new_array() {
+     struct json_array_impl *result = (struct json_array_impl *)malloc(sizeof(json_array_t *));
+     if (!result) return NULL;
+     result->fn.accept    = (json_array_accept_fn)accept;
+     result->fn.count     = (json_array_count_fn )count ;
+     result->fn.get       = (json_array_get_fn   )get   ;
+     result->fn.set       = (json_array_set_fn   )set   ;
+     result->fn.ins       = (json_array_set_fn   )ins   ;
+     result->fn.add       = (json_array_add_fn   )add   ;
+     result->fn.del       = (json_array_del_fn   )del   ;
+     result->capacity     = 0;
+     result->count        = 0;
+     result->values       = NULL;
+     return (json_value_t*)result;
+}
