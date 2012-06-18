3e64768c2514920f4640b4cc099305d464f14bef
Cyril Adrian <cyril.adrian@gmail.com>
2012-06-18 07:27:30 +0200

    more tests

--- test/test.c
+++ test/test.c
@@ -16,14 +16,54 @@
 
 #include "json.h"
 
+static void assert_(int test, const char *message, int line, const char *file) {
+     static int counter = 0;
+     counter++;
+     if (!test) {
+          fprintf(stderr, "Assertion %d (%s:%d) failed: %s\n", counter, file, line, message);
+          exit(counter);
+     }
+}
+
+#define assert(t) assert_((t), #t, __LINE__, __FILE__)
+
 int main() {
      json_object_t *value = json_new_object();
      json_value_t *v;
      json_value_t *t = (json_value_t*)json_const(json_true);
+     json_value_t *f = (json_value_t*)json_const(json_false);
+     json_value_t *n = (json_value_t*)json_const(json_null);
+
+     assert(value->count(value) == 0);
+
      value->set_value(value, "foo", t);
+     assert(value->count(value) == 1);
      v = value->get_value(value, "foo");
-     if (v != t) return 1;
-     if (strcmp("foo", value->get_field(value, 0).key)) return 2;
-     if (value->get_field(value, 0).value != t) return 3;
+     assert(v == t);
+     assert(!strcmp("foo", value->get_field(value, 0).key));
+     assert(value->get_field(value, 0).value == t);
+
+     value->set_value(value, "bar", f);
+     assert(value->count(value) == 2);
+     v = value->get_value(value, "bar");
+     assert(v == f);
+     assert(!strcmp("bar", value->get_field(value, 1).key));
+     assert(value->get_field(value, 1).value == f);
+     v = value->get_value(value, "foo");
+     assert(v == t);
+     assert(!strcmp("foo", value->get_field(value, 0).key));
+     assert(value->get_field(value, 0).value == t);
+
+     value->set_value(value, "foo", n);
+     assert(value->count(value) == 2);
+     v = value->get_value(value, "bar");
+     assert(v == f);
+     assert(!strcmp("bar", value->get_field(value, 1).key));
+     assert(value->get_field(value, 1).value == f);
+     v = value->get_value(value, "foo");
+     assert(v == n);
+     assert(!strcmp("foo", value->get_field(value, 0).key));
+     assert(value->get_field(value, 0).value == n);
+
      return 0;
 }
