f5208be8205910c27201b1c4dec30da07dfafc18
Cyril Adrian <cyril.adrian@gmail.com>
2012-07-03 21:38:38 +0200

    debian packaging, first draft

---
+++ deb-release.sh
@@ -0,0 +1,19 @@
+#!/bin/dash
+
+dir=$(cd $(dirname $(readlink -f $0)); pwd)
+cd $dir
+
+version=$(head -n 1 Changelog | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
+echo "Version is ${version}."
+
+make clean
+
+echo "Saving orig."
+(
+    cd ..
+    tar=libyacjp_${version}.orig.tar.bz2
+    exec tar cfj $tar $(basename $dir)
+)
+
+echo "Starting build."
+exec debuild -us -uc -v${version} "$@"
