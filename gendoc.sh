32a17e62bcc419a5d56961abadc1dbc5de46b925
Cyril Adrian <cyril.adrian@gmail.com>
2012-08-17 19:03:27 +0200

    better debian conformance by now relying on libcad build framework

--- gendoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/dash
-
-mkdir -p target/gendoc
-
-for doc in doc/*.sh; do
-    source=$(basename $doc)
-    target=target/gendoc/${source%.sh}.md
-    $SHELL $doc > $target
-done
--- release.sh
+++ release.sh
@@ -1,19 +1,2 @@
 #!/bin/dash
-
-dir=$(cd $(dirname $(readlink -f $0)); pwd)
-cd $dir
-
-version=$(head -n 1 Changelog | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
-echo "Version is ${version}."
-
-make clean
-
-echo "Saving orig."
-(
-    cd ..
-    tar=libyacjp_${version}.orig.tar.bz2
-    exec tar cfj $tar --exclude-vcs $(basename $dir)
-)
-
-echo "Starting release."
-exec make release
+exec /usr/share/libcad/release.sh "$@"
